bc731d4660a693e686b04f85531a0d969fc800f1
finteger <nashassociatesinc@gmail.com>
2025-05-20 18:38:14 -0600

    Update

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -1,15 +1,22 @@
 const fetchData = require('../functions/api.js');
 
+ //Mock this global fetch function
+global.fetch = jest.fn();
+
 
 //Test Suite
-describe('fetchData', () =>{
-    //Mock this global fetch function
-    global.fetch = jest.fn();
-
-
+describe('fetchData', ()  =>{
+   beforeEach(()=>{
+    //reset the mock object before each test
+    fetch.mockClear();
+   });
 
     //Test cases
-    it('fetches data from an API endpoint and returns it as JSON', () =>{
+    it('fetches data from an API endpoint and returns it as JSON', async () =>{
+
+        //AAA pattern
+
+        //Assemble
         //mock the function response
         const mockResponse = {userId: 1, id: 1, title: 'delectus aut autem', completed: false};
 
@@ -17,6 +24,13 @@ describe('fetchData', () =>{
             ok: true,
             json: jest.fn().mockResolvedValueOnce(mockResponse)
         });
+
+        //Action
+       const data = await fetchData('http://jsonplaceholder.typicode.com/todos/1');
+
+        //Assert
+        
+
     });
 });
 
