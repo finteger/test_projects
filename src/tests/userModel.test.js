b7a89636aef491dea1144030abf14fd6db4cdd39
finteger <nashassociatesinc@gmail.com>
2025-05-27 18:56:03 -0600

    Update

--- src/tests/userModel.test.js
+++ src/tests/userModel.test.js
@@ -8,22 +8,22 @@ describe('User Model Test', () => {
         jest.clearAllMocks;
     });
 
-    it('should create a new user', () =>{
+    it('should create a new user', async () =>{
         //Arrange
         const mockUser = {
             firstName: 'Todd Nash',
-            email: 'todd.nash@rdpolytech',
+            email: 'todd.nash@rdpolytec.ca',
             password: 'password',
-            age: 100
+            age: 100,
         }
-
         //Action
         jest.spyOn(User.prototype, 'save').mockResolvedValue(mockUser);
-        const result = await createUser('Todd Nash', 'todd.nash@rdpolytech', 'password', 100);
-
+        const result = await createUser('Todd Nash', 'todd.nash@rdpolytech.ca', 'password', 100
+        );
+        console.log(result);
+        console.log(mockUser);
         //Assert
         expect(result).toEqual(expect.objectContaining(mockUser));
         expect(User.prototype.save()).toHaveBeenCalledTimes(1);
-
     });
 });
